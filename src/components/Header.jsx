476d41bbf322e06f4eb095fe556c1546c20f58ef
samaahmed1 <sama34777@gmail.com>
2025-02-06 21:29:46 +0200

    Add: Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <a href="index.html">Logo</a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="index.html">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+export default Header;
